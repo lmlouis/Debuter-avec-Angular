6d4f68f73b15afbea73e64a871e341acedee3710
lmlouis <louiswora1@gmail.com>
2023-02-22 16:23:30 +0100

    module routing et navigation

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes : Routes =[
+  {path:'facesnaps', component:FaceSnapListComponent},
+  {path:'', component:LandingPageComponent}
+]
+@NgModule({
+  imports:[RouterModule.forRoot(routes)],
+  exports:[RouterModule]
+}
+)
+export class AppRoutingModule{}
